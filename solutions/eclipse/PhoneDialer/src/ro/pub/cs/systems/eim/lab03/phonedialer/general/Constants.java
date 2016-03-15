e0dafc25efcd55365365737001538c5b03e56de1
Costin-Giorgian PAPUC <papuc.costin@gmail.com>
2016-03-15 18:24:49 +0200

    Added onClickListener for star and pound. Replaced # with %23 for Uri.parse() argument

--- solutions/eclipse/PhoneDialer/src/ro/pub/cs/systems/eim/lab03/phonedialer/general/Constants.java
+++ solutions/eclipse/PhoneDialer/src/ro/pub/cs/systems/eim/lab03/phonedialer/general/Constants.java
@@ -14,6 +14,8 @@ public interface Constants {
             R.id.number_6_button,
             R.id.number_7_button,
             R.id.number_8_button,
-            R.id.number_9_button
+            R.id.number_9_button,
+            R.id.star_button,
+            R.id.pound_button
     };
 }
